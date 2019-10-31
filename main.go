a08cb0bf221c2619238c9aeef7388e52e8532d01
unknown <bureaucratix@gmail.com>
2019-10-31 13:11:23 -0700

    randomized pokemon to pull from API

--- main.go
+++ main.go
@@ -1,28 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
+
+type pokemon struct {
+	Name string `json:"name"`
+}
 
 // https://pokeapi.co/api/v2/pokemon/25
 
-func main() {
-	resp, err := http.Get("https://pokeapi.co/api/v2/pokemon/25")
-
+func errorCheck(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	n1 := rand.Intn(806) + 1
+	resp, apiErr := http.Get(fmt.Sprintf("https://pokeapi.co/api/v2/pokemon/%d", n1))
+
+	errorCheck(apiErr)
 
 	defer resp.Body.Close()
 
-	body, bodyerr := ioutil.ReadAll(resp.Body)
+	body, bodyErr := ioutil.ReadAll(resp.Body)
 
-	if bodyerr != nil {
-		log.Fatalln(bodyerr)
-	}
+	errorCheck(bodyErr)
 
-	fmt.Println(string(body))
+	p := pokemon{}
+	jsonErr := json.Unmarshal(body, &p)
+
+	errorCheck(jsonErr)
+
+	fmt.Println(p.Name)
 }
